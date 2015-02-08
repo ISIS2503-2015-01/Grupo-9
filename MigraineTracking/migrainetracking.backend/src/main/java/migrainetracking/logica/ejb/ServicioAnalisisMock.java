9900ce17e0d808f85d8b3301fc94c0d82ce80e35
Maria Mancipe <mp.mancipe10@uniandes.edu.co>
2015-02-08 12:53:27 -0500

    migrainetracking.backeng y . services

---
+++ MigraineTracking/migrainetracking.backend/src/main/java/migrainetracking/logica/ejb/ServicioAnalisisMock.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package migrainetracking.logica.ejb;
+
+import javax.ejb.Stateless;
+import migrainetracking.logica.interfaces.ServicioAnalisisMockLocal;
+
+/**
+ *
+ * @author estudiante
+ */
+@Stateless
+public class ServicioAnalisisMock implements ServicioAnalisisMockLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
---
+++ MigraineTracking/migrainetracking.backend/src/main/java/migrainetracking/logica/ejb/SevicioRegistroEpisodioMock.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package migrainetracking.logica.ejb;
+
+import javax.ejb.Stateless;
+import migrainetracking.logica.interfaces.SevicioRegistroEpisodioMockLocal;
+
+/**
+ *
+ * @author estudiante
+ */
+@Stateless
+public class SevicioRegistroEpisodioMock implements SevicioRegistroEpisodioMockLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
