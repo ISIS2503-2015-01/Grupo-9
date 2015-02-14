74a6ed1def6c58679a9c019789ba2e13a95c7420
Maria Mancipe <mp.mancipe10@uniandes.edu.co>
2015-02-14 09:18:13 -0500

    Documentacion

--- MigraineTracking/migrainetracking.backend/src/main/java/migrainetracking/dto/Catalizador.java
+++ MigraineTracking/migrainetracking.backend/src/main/java/migrainetracking/dto/Catalizador.java
@@ -1,6 +1,5 @@
 package migrainetracking.dto;
 
-import java.util.Date;
 
 /**
  *
@@ -8,66 +7,103 @@ import java.util.Date;
  */
 public class Catalizador {
     
-    private String tipo; // { alimento,actividad fisica,bebida,habito }
+    //-----------------------------------------------------
+    // Atributos
+    //-----------------------------------------------------
     
+    /**
+     * El id del catalizador
+     */
+    private int id;
+    
+    /**
+     * El tipo del catalizador
+     * Puede ser un alimento, actividad fisica, bebida, o habito
+     */
+    private String tipo; 
+    
+    /**
+     * Especificacion adicional del doctor
+     */
     private String especificacion;
-    
-    private Date ultimaFecha;
-    
-    private String ultimaHora;
     
     /**
      * La cantidad de veces que se ha hecho o ingerido el catalizador
      */
     private int frecuencia;
 
+     //-----------------------------------------------------
+    // Constructores
+    //-----------------------------------------------------
+    
+    /**
+     * Constructor de la clase(sin argumentos)
+     */
     public Catalizador() {
     }
 
-    public Catalizador(String tipo, String especificacion, Date ultimaFecha, String ultimaHora, int frecuencia) {
+    /**
+     * Constructor de la clase(con argumentos)
+     * @param id
+     * @param tipo
+     * @param especificacion
+     * @param frecuencia 
+     */
+    public Catalizador(int id, String tipo, String especificacion, int frecuencia) {
         this.tipo = tipo;
         this.especificacion = especificacion;
-        this.ultimaFecha = ultimaFecha;
-        this.ultimaHora = ultimaHora;
         this.frecuencia = frecuencia;
+        this.id = id;
     }
+    
+     //-----------------------------------------------------
+    // Metodos
+    //-----------------------------------------------------
 
+    /**
+     * Devuelve el tipo del catalizador
+     * @return el tipo
+     */
     public String getTipo() {
         return tipo;
     }
-
+    
+    /**
+     * Modifica el tipo del catalizador
+     * @param tipo puede ser actividad fisica, habito, bebida, alimento
+     */
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
 
+    /**
+     * Devuelve la especificacion del catalizador
+     * @return la especificacion
+     */
     public String getEspecificacion() {
         return especificacion;
     }
 
+    /**
+     * Modifica la especificacion del catalizador
+     * @param especificacion Comentarios que tenga que agregar el doctor
+     */
     public void setEspecificacion(String especificacion) {
         this.especificacion = especificacion;
     }
 
-    public Date getUltimaFecha() {
-        return ultimaFecha;
-    }
-
-    public void setUltimaFecha(Date ultimaFecha) {
-        this.ultimaFecha = ultimaFecha;
-    }
-
-    public String getUltimaHora() {
-        return ultimaHora;
-    }
-
-    public void setUltimaHora(String ultimaHora) {
-        this.ultimaHora = ultimaHora;
-    }
-
+    /**
+     * Devuelve la frecuencia del catalizador
+     * @return la frecuencia
+     */
     public int getFrecuencia() {
         return frecuencia;
     }
-
+    
+    /**
+     * Modifica la frecuencia del catalizador
+     * @param frecuencia un numero de 1 a 10 
+     */
     public void setFrecuencia(int frecuencia) {
         this.frecuencia = frecuencia;
     }
--- MigraineTracking/migrainetracking.backend/src/main/java/migrainetracking/dto/Doctor.java
+++ MigraineTracking/migrainetracking.backend/src/main/java/migrainetracking/dto/Doctor.java
@@ -31,6 +31,7 @@ public class Doctor extends Usuario{
     
     /**
      * La especialidad del doctor
+     * (e.g. neurologia, medicina general)
      */
     private String especialidad;
 
@@ -38,12 +39,28 @@ public class Doctor extends Usuario{
     // Constructores
     //-----------------------------------------------------
     
+    /**
+     * Constructor de la clase
+     * (sin argumentos de la clase doctor, pero con argumentos de la super clase Usuarios)
+     * @param nombre
+     * @param noIdentificacion
+     * @param fechaNacimiento 
+     */
     public Doctor(String nombre, int noIdentificacion, Date fechaNacimiento) {
         super(nombre, noIdentificacion, fechaNacimiento);
         pacientes = new ArrayList<Paciente>();
         colegas = new ArrayList<Doctor>();
     }
 
+    /**
+     * Constructor de la clase(Con todos los argumentos)
+     * @param nombre
+     * @param noIdentificacion
+     * @param fechaNacimiento
+     * @param pacientes
+     * @param colegas
+     * @param especialidad 
+     */
     public Doctor(String nombre, int noIdentificacion, Date fechaNacimiento, List<Paciente> pacientes, List<Doctor> colegas, String especialidad) {
         super(nombre, noIdentificacion, fechaNacimiento);
         this.pacientes = pacientes;
@@ -55,32 +72,51 @@ public class Doctor extends Usuario{
     // Metodos
     //-----------------------------------------------------
     
+    /**
+     * Retorna la lista de pacientes asociados al doctor
+     * @return la lista de pacientes
+     */
     public List<Paciente> getPacientes() {
         return pacientes;
     }
 
+    /**
+     * Modifica la lista de pacientes asociados al doctor
+     * @param pacientes la lista de pacientes
+     */
     public void setPacientes(List<Paciente> pacientes) {
         this.pacientes = pacientes;
     }
 
+    /**
+     * Devuelve la lista de colegas asociados al doctor para dar una segunda opinion
+     * @return la lista de doctores
+     */
     public List<Doctor> getColegas() {
         return colegas;
     }
 
+    /**
+     * Modifica la lista de colegas asociados al doctor
+     * @param colegas la lista de doctores
+     */
     public void setColegas(List<Doctor> colegas) {
         this.colegas = colegas;
     }
 
+    /**
+     * Devuelve la especialidad del doctor
+     * @return la especialidad
+     */
     public String getEspecialidad() {
         return especialidad;
     }
 
+    /**
+     * Modifica la especialidad del doctor
+     * @param especialidad (e.g. neurologia)
+     */
     public void setEspecialidad(String especialidad) {
         this.especialidad = especialidad;
-    }
-    
-    
-    
-    
-    
+    }  
 }
