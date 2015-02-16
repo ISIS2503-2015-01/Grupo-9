9c3be3d7b6105516dabba3e46d05acf556afd050
paotoya757 <pa.otoya757@uniandes.edu.co>
2015-02-16 02:10:28 -0500

    util - funcion imprimir Rosado en consola.

---
+++ MigraineTracking/migrainetracking.backend/src/main/java/migrainetracking/utils/Utils.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package migrainetracking.utils;
+
+/**
+ *
+ * @author Personal
+ */
+public class Utils {
+    public static void printf( String s )
+    {
+       System.out.println( (char)27 + "[35mPersistence: "+s ) ;
+    }
+}
