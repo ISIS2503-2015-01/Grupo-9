da55b5ec58d8d36f5234e1a20a7d4f96ce563037
sabisambra <s.abisambra125@uniandes.edu.co>
2015-02-17 20:10:20 -0500

    Documentacion ServiciosCRUDMock

--- MigraineTracking/migrainetracking.backend/src/main/java/migrainetracking/logica/ejb/ServiciosCRUDMock.java
+++ MigraineTracking/migrainetracking.backend/src/main/java/migrainetracking/logica/ejb/ServiciosCRUDMock.java
@@ -22,24 +22,43 @@ import migrainetracking.logica.interfaces.IServiciosCRUDMockRemote;
 @Stateless
 public class ServiciosCRUDMock implements IServiciosCRUDMockRemote {
 
+    /**
+     * Se encarga de crear los objetos que recibe como parametro
+     * @param o el objeto a crear
+     * @return el id del objeto creado
+     */
     @Override
     public Long create(Object o) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Se encarga de eliminar los objetos que le llegan como parametro
+     * @param o el objeto a eliminar
+     * @return el id del objeto que se elimino
+     */
     @Override
     public Long delete(Object o) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Metodo que edita el objeto que recibe por parametro
+     * @param o el objeto que se va a editar
+     * @return el id del objeto que se edito
+     */
     @Override
     public Long update(Object o) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * Retorna todos los elementos de la clase que se dio como parametro
+     * @param clase la clase de la cual se quieren tener todos los objetos
+     * @return la lista con todos los objetos de la clase que se paso por parametro
+     */
     @Override
     public List<Object> getAll(Class clase) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
 }
