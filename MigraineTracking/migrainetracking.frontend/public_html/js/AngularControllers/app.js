c19f23e73ee61736e358fe70f88fa13b6d62f7df
Maria Mancipe <mp.mancipe10@uniandes.edu.co>
2015-03-22 01:59:57 -0500

    inyeccion de modulos

--- MigraineTracking/migrainetracking.frontend/public_html/js/AngularControllers/app.js
+++ MigraineTracking/migrainetracking.frontend/public_html/js/AngularControllers/app.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-(function(){var app = angular.module('migraineTracking',['registroDoctor']);
+(function(){var app = angular.module('migraineTracking',['registroDoctor', 'registroEpisodio', 'registroPaciente']);
     app.directive('navbar', function(){
         return{
             restrict:'E',
