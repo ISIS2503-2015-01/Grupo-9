3c825b7e9c432e6ed74ef92864a68a7603095380
Maria Mancipe <mp.mancipe10@uniandes.edu.co>
2015-03-22 06:04:09 -0500

    getEpisodios-Listo

--- MigraineTracking/migrainetracking.frontend/public_html/js/AngularControllers/app.js
+++ MigraineTracking/migrainetracking.frontend/public_html/js/AngularControllers/app.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-(function(){var app = angular.module('migraineTracking',['doctorDirectives']);
+(function(){var app = angular.module('migraineTracking',['doctorDirectives','episodioDirectives']);
     app.directive('navbar', function(){
         return{
             restrict:'E',
--- MigraineTracking/migrainetracking.frontend/public_html/js/AngularControllers/episodio.js
+++ MigraineTracking/migrainetracking.frontend/public_html/js/AngularControllers/episodio.js
@@ -3,5 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+(function(){var app1 = angular.module('episodioDirectives',[]);
+    app1.directive('episodioInfo',function(){
+        return{
+            restrict:'E',
+            templateUrl:'partials/episodio-info.html',
+            controller:['$http',function($http){
+                    var self = this;
+                    self.episodios = [];
+                    $http.get('URL').success(function(data){
+                        episodios =data;
+                    });
+            }],
+            controllerAs:'getEpisodios'
+        };
+    });
+    
+})();
 
