25faeaa28d6dcb4df9dbdfb8ee18980c45e30cd1
Maria Mancipe <mp.mancipe10@uniandes.edu.co>
2015-03-22 01:56:06 -0500

    angular min 1.4 beta

--- MigraineTracking/migrainetracking.frontend/public_html/js/AngularControllers/app.js
+++ MigraineTracking/migrainetracking.frontend/public_html/js/AngularControllers/app.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-(function(){var app = angular.module('migraineTracking',[]);
+(function(){var app = angular.module('migraineTracking',['registroDoctor']);
     app.directive('navbar', function(){
         return{
             restrict:'E',
