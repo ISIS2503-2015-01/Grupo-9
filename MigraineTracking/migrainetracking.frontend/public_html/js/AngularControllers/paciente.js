95f37164d19c5dd8807f2944d97b865b4966003f
Maria Mancipe <mp.mancipe10@uniandes.edu.co>
2015-03-22 06:12:48 -0500

    getPacientes-Listo

--- MigraineTracking/migrainetracking.frontend/public_html/js/AngularControllers/paciente.js
+++ MigraineTracking/migrainetracking.frontend/public_html/js/AngularControllers/paciente.js
@@ -3,5 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+(function(){var app2 = angular.module('pacienteDirectives',[]);
+    app2.directive('pacienteInfo',function(){
+        return{
+            restrict:'E',
+            templateUrl:'partials/paciente-info.html',
+            controller:['$http',function($http){
+                    var self = this;
+                    self.pacientes =[];
+                    $http.get('URL').success(function(data){
+                        pacientes =data;
+                    });
+            }],
+            controllerAs:'getPacientes'
+        };
+    });
+    
+})();
 
