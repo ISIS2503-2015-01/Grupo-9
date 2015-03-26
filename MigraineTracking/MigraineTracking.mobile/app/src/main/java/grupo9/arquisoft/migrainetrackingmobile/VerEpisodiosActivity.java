d7d3eab6f77d2150f0241ea0eb26ba88746aea69
henryfvargas22 <henryfvargas@MacBook-Pro-de-Henry.local>
2015-03-25 23:48:58 -0500

    cambios en ver episodios
    se pone una lista temporal para albergar los episodios

--- MigraineTracking/MigraineTracking.mobile/app/src/main/java/grupo9/arquisoft/migrainetrackingmobile/VerEpisodiosActivity.java
+++ MigraineTracking/MigraineTracking.mobile/app/src/main/java/grupo9/arquisoft/migrainetrackingmobile/VerEpisodiosActivity.java
@@ -5,14 +5,63 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class VerEpisodiosActivity extends ActionBarActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_episodios);
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.listView2);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "2014-02-03: Episodio grabado.",
+                "2014-02-10: Episodio escrito.",
+                "2014-02-19: Episodio grabado."
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });
         Intent intent = getIntent();
     }
 
